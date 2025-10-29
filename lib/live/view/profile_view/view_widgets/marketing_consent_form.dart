f095dff97efe278ee21517be5518d342549eab6f
Tiisetso Dinoko <dinokovonteese@gmail.com>
2025-10-29 15:32:29 +0200

    fix: save marketing and telephone suvey consents

--- lib/live/view/profile_view/view_widgets/marketing_consent_form.dart
+++ lib/live/view/profile_view/view_widgets/marketing_consent_form.dart
@@ -1,75 +1,79 @@
 import 'package:flutter/material.dart';
 import '../../../../utils/dimensions.dart';
 import '../../../widgets/text_widgets/heading_style_text.dart';
+import '../controller/profile_view_controller.dart';
+import 'package:provider/provider.dart';
 
-class MarketingConsentForm extends StatefulWidget {
+class MarketingConsentForm extends StatelessWidget {
   final String description;
-  late bool isSelected;
+  final bool isEmailMarketing;
   final FontWeight fontWeight;
 
-  MarketingConsentForm({
+  const MarketingConsentForm({
     Key? key,
     required this.description,
-    required this.isSelected,
+    required this.isEmailMarketing,
     required this.fontWeight,
   }) : super(key: key);
 
   @override
-  State<MarketingConsentForm> createState() => _MarketingConsentFormState();
-}
-
-class _MarketingConsentFormState extends State<MarketingConsentForm> {
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Row(
-          crossAxisAlignment:
-              CrossAxisAlignment.start, // Align items at the start (top)
+    return Consumer<ProfileViewController>(
+      builder: (context, controller, child) {
+        bool isSelected = isEmailMarketing
+            ? controller.emailMarketingConsent
+            : controller.telephoneSurveyConsent;
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Transform.scale(
-              scale: 1.1,
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  unselectedWidgetColor: Colors.black, // Darker border color
-                  checkboxTheme: CheckboxThemeData(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(2.0), // Square shape
-                      side: BorderSide(
-                        color: Colors.black, // Darker border
-                        width: 1.0,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Transform.scale(
+                  scale: 1.1,
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                      unselectedWidgetColor: Colors.black,
+                      checkboxTheme: CheckboxThemeData(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(2.0),
+                          side: BorderSide(color: Colors.black, width: 1.0),
+                        ),
                       ),
+                    ),
+                    child: Checkbox(
+                      value: isSelected,
+                      onChanged: (bool? value) {
+                        if (isEmailMarketing) {
+                          controller
+                              .updateEmailMarketingConsent(value ?? false);
+                        } else {
+                          controller
+                              .updateTelephoneSurveyConsent(value ?? false);
+                        }
+                      },
                     ),
                   ),
                 ),
-                child: Checkbox(
-                  value: widget.isSelected,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      widget.isSelected = value ?? false;
-                    });
-                    // Additional actions here if needed
-                  },
+                SizedBox(width: 8.0),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: Dimensions.height10),
+                    child: HeadingStyleText(
+                      text: description,
+                      size: Dimensions.font20 / 1.3,
+                      family: 'Poppins',
+                      weight: fontWeight,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(width: 8.0), // Spacing between the checkbox and text
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(top: Dimensions.height10),
-                child: HeadingStyleText(
-                  text: widget.description,
-                  size: Dimensions.font20 / 1.3,
-                  family: 'Poppins',
-                  weight: widget.fontWeight,
-                  color: Colors.black,
-                ),
-              ),
+              ],
             ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }

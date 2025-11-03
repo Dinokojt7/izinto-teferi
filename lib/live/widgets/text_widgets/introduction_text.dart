4e7d1e56fdd025dbb7c9b474636ad08d1de05eb5
Tiisetso Dinoko <dinokovonteese@gmail.com>
2025-11-03 17:09:10 +0200

    fix introduction text right overflow

--- lib/live/widgets/text_widgets/introduction_text.dart
+++ lib/live/widgets/text_widgets/introduction_text.dart
@@ -7,22 +7,26 @@ class IntroductionText extends StatelessWidget {
   final double? textSize;
   final int? maxLines;
   final Color? color;
-  const IntroductionText(
-      {super.key,
-      required this.text,
-      this.textSize,
-      this.maxLines = 2,
-      this.color = Colors.black});
+  const IntroductionText({
+    super.key,
+    required this.text,
+    this.textSize,
+    this.maxLines = 2,
+    this.color = Colors.black,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      constraints: BoxConstraints(
+        maxWidth: Dimensions.screenWidth - 40, // Account for padding
+      ),
       child: Text(
         text,
         maxLines: maxLines,
+        overflow: TextOverflow.ellipsis,
         style: TextStyle(
           height: 1.2,
-          overflow: TextOverflow.ellipsis,
           fontSize: textSize ?? Dimensions.font26,
           fontFamily: 'Poppins',
           color: color,
